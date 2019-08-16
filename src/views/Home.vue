1381e112a561db20cd836e0b4a949aaa6abc8fce
JeanRM <jean.martins0000@gmail.com>
2019-08-16 09:32:52 -0400

    fotos do projeto no carousel . DEU CERTO.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,50 +1,37 @@
 <template>
-  <v-carousel
-    cycle
-    height="400"
-    hide-delimiter-background
-    show-arrows-on-hover
-  >
+  <v-carousel hide-delimiters>
     <v-carousel-item
-      v-for="(slide, i) in slides"
+      v-for="(item,i) in items"
       :key="i"
-    >
-      <v-sheet
-        :color="colors[i]"
-        height="100%"
-      >
-        <v-layout
-          align-center
-          fill-height
-          justify-center
-        >
-          <div class="display-3">{{ slide }} Slide</div>
-        </v-layout>
-      </v-sheet>
-    </v-carousel-item>
+      :src="item.src"
+    ></v-carousel-item>
   </v-carousel>
 </template>
+  
 
 
-<script>
-  export default {
-    data () {
-      return {
-        colors: [
-          'indigo',
-          'warning',
-          'pink darken-2',
-          'red lighten-1',
-          'deep-purple accent-4',
-        ],
-        slides: [
-          'First',
-          'Second',
-          'Third',
-          'Fourth',
-          'Fifth',
-        ],
-      }
-    },
-  }
-</script>
+  <script>
+    export default {
+      data () {
+        return {
+          items: [
+            {
+              src: require('../assets/estudos.jpg'),
+            },
+            {
+              src: require('../assets/fetec.jpg'),
+            },
+            {
+              src: require('../assets/ismac.jpg'),
+            },
+            {
+              src: require('../assets/orientador.jpg'),
+            },
+            {
+              src: require('../assets/premiacao.jpg'),
+            },
+          ],
+        }
+      },
+    }
+  </script>
